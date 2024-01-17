787b0ccd9e9c001e3a9bd924cdedcc4361e0f4fe
fatur <faturromansetiyono@gmail.com>
2024-01-17 20:31:40 +0700

    Develop task create

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,35 +1,61 @@
 @extends('layouts.master')
+
 @section('pageTitle', $pageTitle)
 
 @section('main')
-<div class="form-container">
+  <div class="form-container">
     <h1 class="form-title">{{ $pageTitle }}</h1>
-    <form class="form">
+    <form class="form" method="POST" action="{{ route('tasks.store') }}">
+      @csrf
       <div class="form-item">
         <label>Name:</label>
-        <input class="form-input" type="text" value="" >
+        <input class="form-input" type="text" name="name" value="{{ old('name') }}">
+        <!-- Menampilkan pesan error untuk name -->
+        @error('name')
+          <div class="alert-danger">{{ $message }}</div>
+        @enderror
       </div>
 
       <div class="form-item">
         <label>Detail:</label>
-        <textarea class="form-text-area"></textarea>
+        <textarea class="form-text-area" name="detail">{{ old('detail') }}</textarea>
+         <!-- Menampilkan pesan error untuk detail -->
+        @error('detail')
+            <div class="alert-danger">{{ $message }}</div>
+        @enderror
       </div>
 
       <div class="form-item">
         <label>Due Date:</label>
-        <input class="form-input" type="date" value="" >
+        <input class="form-input" type="date" name="due_date" value="{{ old('due_date') }}">
+        <!-- Menampilkan pesan error untuk due_date -->
+        @error('due_date')
+          <div class="alert-danger">{{ $message }}</div>
+        @enderror
       </div>
 
       <div class="form-item">
         <label>Progress:</label>
-        <select class="form-input">
-          <option value="not_started">Not Started</option>
-          <option value="in_progress">In Progress</option>
-          <option value="in_review">Waiting/In Review</option>
-          <option value="completed">Completed</option>
-        </select>
+        <select class="form-input" name="status">
+            <option @if(old('status') == 'not_started') selected @endif value="not_started">
+              Not Started
+            </option>
+            <option @if(old('status') == 'in_progress') selected @endif value="in_progress">
+              In Progress
+            </option>
+            <option @if(old('status') == 'in_review') selected @endif value="in_review">
+              Waiting/In Review
+            </option>
+            <option @if(old('status') == 'completed') selected @endif value="completed">
+              Completed
+            </option>
+          </select>
+        <!-- Menampilkan pesan error untuk status -->
+        @error('status')
+          <div class="alert-danger">{{ $message }}</div>
+        @enderror
       </div>
-      <button type="button" class="form-button">Submit</button>
+      <button type="submit" class="form-button">Submit</button>
     </form>
   </div>
-  @endsection
+@endsection
