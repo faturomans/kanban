0b306bb734e50bce3958330473a63a1e21cab0a4
fatur <faturromansetiyono@gmail.com>
2024-01-17 21:26:38 +0700

    Update Terbaru CRUD

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,87 +2,91 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     private $tasks;
 
-    public function __construct()
-    {
-        $this->tasks = [
-            (object) [
-                'id' => 1,
-                'name' => 'Develop Final Project',
-                'detail' => 'Kanban project using PHP and Laravel',
-                'due_date' => '2023-04-30',
-                'status' => 'not_started',
-            ],
-            (object) [
-                'id' => 2,
-                'name' => 'Lunch with Guru Domba',
-                'detail' => 'Have Nasi Padang with Guru Domba',
-                'due_date' => '2023-04-10',
-                'status' => 'not_started',
-            ],
-            (object) [
-                'id' => 3,
-                'name' => 'Learn Blade Templating',
-                'detail' => 'Complete Blade Templating material on Progate',
-                'due_date' => '2023-04-05',
-                'status' => 'in_progress',
-            ],
-            (object) [
-                'id' => 4,
-                'name' => 'Decide Plans for Lebaran holidays',
-                'detail' => 'Trip with family?',
-                'due_date' => '2023-04-21',
-                'status' => 'in_progress',
-            ],
-            (object) [
-                'id' => 5,
-                'name' => 'Develop a Laravel Project',
-                'detail' => 'Develop a Kanban app and ask Guru Domba\'s review',
-                'due_date' => '2023-04-30',
-                'status' => 'in_review',
-            ],
-            (object) [
-                'id' => 6,
-                'name' => 'Learn PHP Basics',
-                'detail' => 'Complete PHP materials on Frontend Course',
-                'due_date' => '2023-04-30',
-                'status' => 'completed',
-            ],
-        ];
-    }
+    // public function __construct()
+    // {
+
+    // }
 
     public function index()
-{
-    $pageTitle = 'Task List'; // Ditambahkan
-    $tasks = $this->tasks;
-    return view('tasks.index', [
-        'pageTitle' => $pageTitle, //Ditambahkan
-        'tasks' => $tasks,
-    ]);
-}
+    {
+        $pageTitle = 'Task List'; // Ditambahkan
+        $tasks = Task::all(); // Diperbarui
+        return view('tasks.index', [
+            'pageTitle' => $pageTitle, //Ditambahkan
+            'tasks' => $tasks,
+        ]);
+    }
+    public function create()
+    {
+        $pageTitle = 'Create Task';
+        return view('tasks.create', ['pageTitle' => $pageTitle]);
+    }
 
-public function create()
-{
-    $pageTitle = 'Task List'; // Ditambahkan
-    $tasks = $this->tasks;
-    return view('tasks.create', [
-        'pageTitle' => $pageTitle, //Ditambahkan
-        'tasks' => $tasks,
-    ]);
-}
 
-public function edit($id)
-{
-    $pageTitle = 'Edit Task';
-    $tasks = $this->tasks;
+    public function store(Request $request)
+    {
+        $request->validate(
+            [
+                'name' => 'required',
+                'detail' => 'required',
+                'due_date' => 'required',
+                'status' => 'required',
+            ],
+            $request->all()
+        );
 
-    $task = $tasks[$id - 1];
+        $task = new Task([
+            'name' => $request->name,
+            'detail' => $request->detail,
+            'due_date' => $request->due_date,
+            'status' => $request->status,
+        ]);
 
-    return view('tasks.edit', ['pageTitle' => $pageTitle, 'task' => $task]);
-}
+        $task->save();
+
+        return redirect()->route('tasks.index');
+    }
+
+    public function edit($id)
+    {
+        $pageTitle = 'Edit Task';
+        $task = Task::find($id);
+        return view('tasks.edit', ['pageTitle' => $pageTitle, 'task' => $task]);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+
+        $this->validate($request, [
+            'name' => 'required',
+            'detail' => 'required',
+            'due_date' => 'required|date',
+            'status' => 'required|in:not_started,in_progress,in_review,completed',
+        ]);
+
+        $task = Task::find($id);
+        $task->name = $request->input('name');
+        $task->detail = $request->input('detail');
+        $task->due_date = $request->input('due_date');
+        $task->status = $request->input('status');
+        $task->save();
+
+        return redirect()->route('tasks.index')->with('success', 'Task updated successfully');
+    }
+
+
+    public function delete($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+        return redirect()->route('tasks.index');
+    }
 }
